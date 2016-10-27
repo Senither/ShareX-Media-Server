ac3d2decba9d574004ef12755d4483f9f5c21741
Alexis Tan <alexis@sen-dev.com>
2016-10-27 02:36:53 +0200

    Add support for quick-swaping between HTTP and HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (config('app.secure')) {
+            URL::forceSchema('https');
+        }
     }
 
     /**

2531d7856c213e23cdd5d22f11f44a5f24c2df14
Alexis Tan <alexis@sen-dev.com>
2021-02-22 03:58:02 +0100

    Change landing page to redirect to the login page

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/login');
 
 Route::get('i/{image}/{type?}', RenderImageController::class)->name('view-image');
 

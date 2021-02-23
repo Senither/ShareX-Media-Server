9073531f56f2886a3a0462267a0c1cca5042495f
Alexis Tan <alexis@sen-dev.com>
2021-02-23 02:02:33 +0100

    Fix render image not using user scope for finding images

--- app/Http/Controllers/RenderImageController.php
+++ app/Http/Controllers/RenderImageController.php
@@ -3,14 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\Image;
+use App\Scopes\UserScope;
 use GuzzleHttp\Psr7\MimeType;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
 class RenderImageController extends Controller
 {
-    public function __invoke(Image $image, $type = null)
+    /**
+     * Invokes the method automatically when the
+     * class is called as a function.
+     *
+     * @param  string $id
+     * @param  string|null $type
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke($id, $type = null)
     {
+        $image = $this->loadImageOrFail($id);
+
         $path = 'images/' . $image->getResourceName($type);
 
         if (!Storage::exists($path)) {
@@ -19,5 +30,18 @@ class RenderImageController extends Controller
 
         return response(Storage::get($path))
             ->header('Content-Type', MimeType::fromExtension($image->extension));
+    }
+
+    /**
+     * Loads the image with the given name, or fails trying.
+     *
+     * @param  string $name
+     * @return \App\Models\Image
+     */
+    protected function loadImageOrFail($name)
+    {
+        return Image::withoutGlobalScope(UserScope::class)
+            ->where('name', $name)
+            ->first();
     }
 }

8670403521cd287555fc0cb389f999a39a7fe8cf
Alexis Tan <alexis@sen-dev.com>
2021-02-25 15:23:48 +0100

    Remove impersonating button

--- resources/views/control-panel/user-management-entity.blade.php
+++ resources/views/control-panel/user-management-entity.blade.php
@@ -3,22 +3,6 @@
     <div class="p-4 col-span-5 text-left text-sm text-gray-500">{{ $user->email }}</div>
     <div class="p-4 col-span-3 text-left text-sm text-gray-500">
         <div class="flex space-x-1">
-            @if(!$user->is_admin)
-                <button class="border-2 border-indigo-200 rounded-md p-1 focus:outline-none">
-                    <!-- Heroicons: users -->
-                    <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-indigo-500">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
-                    </svg>
-                </button>
-            @else
-                <button class="border-2 border-gray-200 rounded-md p-1 focus:outline-none cursor-not-allowed">
-                    <!-- Heroicons: users -->
-                    <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-500">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
-                    </svg>
-                </button>
-            @endif
-
             <button wire:click="$toggle('confirmingUserEditing')" class="border-2 border-indigo-200 rounded-md p-1 focus:outline-none">
                 <!-- Heroicons: pencil -->
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-indigo-500">

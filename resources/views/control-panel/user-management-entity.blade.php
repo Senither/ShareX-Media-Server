6ef54c43aef92d0776dd4c5e60bdf9f9ce9f7583
Alexis Tan <alexis@sen-dev.com>
2021-02-25 23:47:11 +0100

    Fix impersonating icon

--- resources/views/control-panel/user-management-entity.blade.php
+++ resources/views/control-panel/user-management-entity.blade.php
@@ -12,9 +12,9 @@
                 </button>
             @else
                 <button class="border-2 border-gray-200 rounded-md p-1 focus:outline-none cursor-not-allowed">
-                    <!-- Heroicons: trash -->
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-gray-500">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                    <!-- Heroicons: users -->
+                    <svg class="h-4 w-4 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
                     </svg>
                 </button>
             @endif

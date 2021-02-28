4eb130419ac73dbc635642d186780507fddd1276
Alexis Tan <alexis@sen-dev.com>
2021-02-28 15:42:44 +0100

    Make image preview list header text smaller

--- resources/views/images/image-preview-list.blade.php
+++ resources/views/images/image-preview-list.blade.php
@@ -2,7 +2,7 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
             <div class="p-6 flex items-center justify-between bg-white border-b border-gray-200">
-                <div class="text-2xl">
+                <div class="text-xl">
                     Latest image uploads
                 </div>
 

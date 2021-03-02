87a76cd5b17ec42e0bbfdbc5053d7ea67add53ab
Alexis Tan <alexis@sen-dev.com>
2021-03-02 21:33:12 +0100

    Convert user profile and API views

--- resources/views/vendor/jetstream/components/action-section.blade.php
+++ resources/views/vendor/jetstream/components/action-section.blade.php
@@ -5,7 +5,7 @@
     </x-jet-section-title>
 
     <div class="mt-5 md:mt-0 md:col-span-2">
-        <div class="px-4 py-5 sm:p-6 bg-white shadow sm:rounded-lg">
+        <div class="px-4 py-5 sm:p-6 bg-white dark:bg-dark-gray-700 dark:text-dark-gray-200 shadow sm:rounded-lg">
             {{ $content }}
         </div>
     </div>

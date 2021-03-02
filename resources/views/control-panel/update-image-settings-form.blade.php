7533eb75cdf0d7356b8e6787fbfdc59f3c865fe1
Alexis Tan <alexis@sen-dev.com>
2021-03-02 21:14:58 +0100

    Make control panel IDs more unique

--- resources/views/control-panel/update-image-settings-form.blade.php
+++ resources/views/control-panel/update-image-settings-form.blade.php
@@ -10,11 +10,11 @@
     <x-slot name="form">
         <!-- TTL -->
         <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="ttl_days" value="{{ __('Images Live Time (Days, Hours and Minutes)') }}" />
+            <x-jet-label for="image_ttl_days" value="{{ __('Images Live Time (Days, Hours and Minutes)') }}" />
             <div class="flex space-x-3">
-                <x-jet-input id="ttl_days" type="number" class="mt-1 block w-full" min="0" wire:model.defer="settings.ttl_days" />
-                <x-jet-input id="ttl_hours" type="number" class="mt-1 block w-full" min="0" wire:model.defer="settings.ttl_hours" />
-                <x-jet-input id="ttl_minutes" type="number" class="mt-1 block w-full" min="0" wire:model.defer="settings.ttl_minutes" />
+                <x-jet-input id="image_ttl_days" type="number" class="mt-1 block w-full" min="0" wire:model.defer="settings.ttl_days" />
+                <x-jet-input id="image_ttl_hours" type="number" class="mt-1 block w-full" min="0" wire:model.defer="settings.ttl_hours" />
+                <x-jet-input id="image_ttl_minutes" type="number" class="mt-1 block w-full" min="0" wire:model.defer="settings.ttl_minutes" />
             </div>
             <x-jet-input-error for="settings.ttl_days" class="mt-2" />
             <x-jet-input-error for="settings.ttl_hours" class="mt-2" />
@@ -23,8 +23,8 @@
 
         <!-- Per page -->
         <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="per_page" value="{{ __('Images per page') }}" />
-            <x-jet-input id="per_page" type="number" class="mt-1 block w-full" min="1" wire:model.defer="settings.per_page" />
+            <x-jet-label for="image_per_page" value="{{ __('Images per page') }}" />
+            <x-jet-input id="image_per_page" type="number" class="mt-1 block w-full" min="1" wire:model.defer="settings.per_page" />
             <x-jet-input-error for="settings.per_page" class="mt-2" />
         </div>
     </x-slot>

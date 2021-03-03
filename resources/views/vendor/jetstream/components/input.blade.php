8f471145a0b6e350daa4f1c632159699a95aa8a8
Alexis Tan <alexis@sen-dev.com>
2021-03-03 14:37:08 +0100

    Improve input styles for light and dark mode

--- resources/views/vendor/jetstream/components/input.blade.php
+++ resources/views/vendor/jetstream/components/input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'text-dark-gray-200 border-gray-300 dark:border-dark-gray-600 focus:border-indigo-300 dark:focus:border-dark-gray-600 focus:ring dark:focus:ring-dark-gray-800 focus:ring-opacity-50 dark:bg-dark-gray-800 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'dark:text-dark-gray-200 border-gray-300 dark:border-dark-gray-600 focus:border-indigo-300 dark:focus:border-dark-gray-600 focus:ring-1 dark:focus:ring-dark-gray-800 dark:bg-dark-gray-800 focus:ring-opacity-50 rounded-md shadow-sm']) !!}>

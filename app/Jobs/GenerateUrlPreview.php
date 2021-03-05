7ea59821bd2560b128f30d5463eb229d6a5548fe
Alexis Tan <alexis@sen-dev.com>
2021-03-05 14:46:57 +0100

    Create URL preview generator using Browsershot

---
+++ app/Jobs/GenerateUrlPreview.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
+use Spatie\Browsershot\Browsershot;
+use Spatie\Image\Manipulations;
+
+class GenerateUrlPreview implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * The URL that the preview should be generated for.
+     *
+     * @var string
+     */
+    public $url;
+
+    /**
+     * The number of seconds the job can run before timing out.
+     *
+     * @var int
+     */
+    public $timeout = 30;
+
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 5;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param  $url
+     * @return void
+     */
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if (!Storage::exists('url-previews')) {
+            Storage::makeDirectory('url-previews');
+        }
+
+        Browsershot::url($this->url)
+            ->noSandbox()
+            ->windowSize(640, 480)
+            ->fit(Manipulations::FIT_CONTAIN, 256, 256)
+            ->setScreenshotType('jpeg', 100)
+            ->save(storage_path('app/url-previews/some-unique-id.jpg'));
+    }
+}
